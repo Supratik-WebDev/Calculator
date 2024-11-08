d255e80fc9ae0537a2ac54801d8d1d8c8a8f10b8
Supratik Roy <roysupratik23@outlook.com>
2024-11-08 20:33:15 +0530

    I change full code and files

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
-function appendNumber(number) {
-    document.getElementById('display').value += number;
-}
+let display = document.getElementById("display");
 
-function appendOperator(operator) {
-    document.getElementById('display').value += ' ' + operator + ' ';
-}
-
+// Function to clear the display
 function clearDisplay() {
-    document.getElementById('display').value = '';
+    display.value = "";
 }
 
+// Function to delete the last character
+function deleteLast() {
+    display.value = display.value.slice(0, -1);
+}
+
+// Function to append a number to the display
+function appendNumber(number) {
+    display.value += number;
+}
+
+// Function to append an operator to the display
+function appendOperator(operator) {
+    if (display.value !== "" && !isNaN(display.value.slice(-1))) {
+        display.value += operator;
+    }
+}
+
+// Function to calculate the result
 function calculateResult() {
-    const display = document.getElementById('display').value;
-    const result = eval(display);
-    document.getElementById('display').value = result;
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = "Error";
+    }
 }
